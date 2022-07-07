69e0fa07e859367b899f834fc3cc34edd5a4a2bc
Ajith <ajith@Ajiths-MacBook-Pro.local>
2022-07-07 18:31:10 +0530

    fix routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import './index.css'
 import Modules from './modules'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Modules />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+const container = document.getElementById('root')
+const root = createRoot (container!)
+
+
+root.render(<React.StrictMode>
+  <Modules />
+</React.StrictMode>)
+
--- src/modules/index.tsx
+++ src/modules/index.tsx
@@ -1,17 +1,11 @@
-import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-  } from "react-router-dom";
+
 import routes from '../common/config/routes'
 import Home from './Home';
 
 const Routing = () => {
-    return <Router>
-        <Routes>
-          <Route path={routes.HOME} element={<Home />} />
-        </Routes>
-    </Router>
+    return <>
+      <Home />
+   </>
 }
 
 export default Routing
